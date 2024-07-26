6bce3d6a90b66ba8dbce3d98d9d9d8df596e1186
Élaff Ramos <valelaff10@gmail.com>
2024-07-26 18:49:12 -0300

    Update raiz.py

--- raiz.py
+++ raiz.py
@@ -1,5 +1,8 @@
 #from math import sqrt   #Desse modo só importaria a raiz quadrada, mas sem as outras funcionalidades da biblioteca.
+
 import math #Biblioteca de matemática
+
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num) #Para saber qual é a raiz quadrada do número digitado.
+
 print('A raiz quadrada do número {} é {}'.format(num, raiz))
